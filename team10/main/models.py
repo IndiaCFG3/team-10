0fd78ee9f7695b349e73d4e5a9f22e2f56663b9f
vaishnavi-kamineni13 <vaishnavikamineni@gmail.com>
2020-08-16 18:10:30 +0530

    admin

--- team10/main/models.py
+++ team10/main/models.py
@@ -12,3 +12,9 @@ class userSignup(models.Model):
     email = models.EmailField(max_length=254)
     password=models.CharField(max_length=30)
     university=models.CharField(max_length=100)
+class courses(models.Model):
+    coursename=models.CharField(max_length=100)
+    description=models.CharField(max_length=100)
+    videolink=models.CharField(max_length=500)
+    pdfdrivelink=models.CharField(max_length=1000)
+    category=models.CharField(max_length=50)
